af04d4594a3ff35c88829455d3e251ae02d617ef
tvjj10 <tvjj10@gmail.com>
2023-04-04 20:12:05 -0400

    Icontact interface added

---
+++ IContact.cs
@@ -0,0 +1,7 @@
+// namning convention is to start interface names with an uppercase I
+
+interface IContact {
+    //best pratice > keep interfaces short. define as few methods as we can.
+    string ShowContatcInfo(); //if a class is a contact, it must show their info.
+    
+}
